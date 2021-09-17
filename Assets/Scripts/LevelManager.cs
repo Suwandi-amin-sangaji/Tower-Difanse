5a8874ed9a1414142c31cdb469a92a9420ddee46
suwandi-amin-sangaji <suwandiaminsangaji@gmail.com>
2021-09-17 18:25:59 +0900

    Update LevelManager.cs
    menambahkan exit pada Escape

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,32 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class LevelManager : MonoBehaviour
-{
+public class LevelManager : MonoBehaviour{
+
+    public bool IsOver { get; private set; }
+
     // Fungsi Singleton
     private static LevelManager _instance = null;
-    public static LevelManager Instance
-    {
-        get
-        {
-            if (_instance == null)
-            {
-                _instance = FindObjectOfType<LevelManager> ();
+    public static LevelManager Instance {
+        get {
+            if(_instance == null) {
+                _instance = FindObjectOfType<LevelManager>();
             }
 
             return _instance;
         }
     }
-
-    [SerializeField] private int _maxLives = 3;
-    [SerializeField] private int _totalEnemy = 15;
-
-    [SerializeField] private GameObject _panel;
-    [SerializeField] private Text _statusInfo;
-    [SerializeField] private Text _livesInfo;
-    [SerializeField] private Text _totalEnemyInfo;
 
     [SerializeField] private Transform _towerUIParent;
     [SerializeField] private GameObject _towerUIPrefab;
@@ -37,41 +29,43 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private Transform[] _enemyPaths;
     [SerializeField] private float _spawnDelay = 5f;
 
-    private List<Tower> _spawnedTowers = new List<Tower> ();
-    private List<Enemy> _spawnedEnemies = new List<Enemy> ();
-    private List<Bullet> _spawnedBullets = new List<Bullet> ();
+    [SerializeField] private int _maxLives = 3;
+    [SerializeField] private int _totalEnemy = 15;
+    [SerializeField] private GameObject _panel;
+    [SerializeField] private Text _statusInfo;
+    [SerializeField] private Text _livesInfo;
+    [SerializeField] private Text _totalEnemyInfo;
 
+    private List<Tower> _spawnedTowers = new List<Tower>();
+    private List<Enemy> _spawnedEnemies = new List<Enemy>();
+    private List<Bullet> _spawnedBullets = new List<Bullet>();
+
+    private float _runningSpawnDelay;
     private int _currentLives;
     private int _enemyCounter;
-    private float _runningSpawnDelay;
-    private AudioSource audioSource;
 
-    public bool IsOver { get; private set; }
 
-    public AudioSource AudioSource
-    {
-        get { return audioSource; }
+    // Start is called before the first frame update
+    void Start(){
+        SetCurrentLives(_maxLives);
+        SetTotalEnemy(_totalEnemy);
+
+        InstantiateAllTowerUI();
     }
 
+    // Update is called once per frame
+    private void Update() {
 
-    private void Start ()
-    {
-        SetCurrentLives (_maxLives);
-        SetTotalEnemy (_totalEnemy);
-
-        InstantiateAllTowerUI ();
-    }
-
-    private void Update ()
-    {
         // Jika menekan tombol R, fungsi restart akan terpanggil
-        if (Input.GetKeyDown (KeyCode.R))
-        {
-            SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+        if (Input.GetKeyDown(KeyCode.R)) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
 
-        if (IsOver)
-        {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+        }
+
+        if (IsOver) {
             return;
         }
 
@@ -79,177 +73,151 @@ public class LevelManager : MonoBehaviour
         // Time.unscaledDeltaTime adalah deltaTime yang independent, tidak terpengaruh oleh apapun kecuali game object itu sendiri,
         // jadi bisa digunakan sebagai penghitung waktu
         _runningSpawnDelay -= Time.unscaledDeltaTime;
-        if (_runningSpawnDelay <= 0f)
-        {
-            SpawnEnemy ();
+
+        if (_runningSpawnDelay <= 0f) {
+            SpawnEnemy();
             _runningSpawnDelay = _spawnDelay;
         }
 
-        foreach (Tower tower in _spawnedTowers)
-        {
-            tower.CheckNearestEnemy (_spawnedEnemies);
-            tower.SeekTarget ();
-            tower.ShootTarget ();
+        foreach (Tower tower in _spawnedTowers) {
+            tower.CheckNearestEnemy(_spawnedEnemies);
+            tower.SeekTarget();
+            tower.ShootTarget();
         }
 
-        foreach (Enemy enemy in _spawnedEnemies)
-        {
-            if (!enemy.gameObject.activeSelf)
-            {
+        foreach (Enemy enemy in _spawnedEnemies) {
+            if (!enemy.gameObject.activeSelf) {
                 continue;
             }
 
             // Kenapa nilainya 0.1? Karena untuk lebih mentoleransi perbedaan posisi,
             // akan terlalu sulit jika perbedaan posisinya harus 0 atau sama persis
-            if (Vector2.Distance (enemy.transform.position, enemy.TargetPosition) < 0.1f)
-            {
-                enemy.SetCurrentPathIndex (enemy.CurrentPathIndex + 1);
-                if (enemy.CurrentPathIndex < _enemyPaths.Length)
-                {
-                    enemy.SetTargetPosition (_enemyPaths[enemy.CurrentPathIndex].position);
+            if (Vector2.Distance(enemy.transform.position, enemy.TargetPos) < 0.1f) {
+                enemy.SetCurrentPathIndex(enemy.CurrentPathIndex + 1);
+                if (enemy.CurrentPathIndex < _enemyPaths.Length) {
+                    enemy.SetTargetPos(_enemyPaths[enemy.CurrentPathIndex].position);
+                } else {
+                    ReduceLives(1);
+                    enemy.gameObject.SetActive(false);
                 }
-                else
-                {
-                    ReduceLives (1);
-                    enemy.gameObject.SetActive (false);
-                }
-            }
-            else
-            {
-                enemy.MoveToTarget ();
+            } else {
+                enemy.MoveToTarget();
             }
         }
     }
 
-    private void InstantiateAllTowerUI ()
-    {
-        foreach (Tower tower in _towerPrefabs)
-        {
-            GameObject newTowerUIObj = Instantiate (_towerUIPrefab.gameObject, _towerUIParent);
-            TowerUI newTowerUI = newTowerUIObj.GetComponent<TowerUI> ();
+    // Menampilkan seluruh tower yang tersedia pada UI Tower Selection
+    private void InstantiateAllTowerUI() {
+        foreach(Tower tower in _towerPrefabs) {
+            GameObject newTowerUIObj = Instantiate(_towerUIPrefab.gameObject, _towerUIParent);
+            TowerUI newTowerUI = newTowerUIObj.GetComponent<TowerUI>();
 
-            newTowerUI.SetTowerPrefab (tower);
+            newTowerUI.SetTowerPrefab(tower);
             newTowerUI.transform.name = tower.name;
         }
     }
 
-    public void RegisterSpawnedTower (Tower tower)
-    {
-        _spawnedTowers.Add (tower);
+    // Mendaftarkan Tower yang di-spawn agar bisa dikontrol oleh LevelManager
+    public void RegisterSpawnedTower(Tower tower) {
+        _spawnedTowers.Add(tower);
     }
 
-    private void SpawnEnemy ()
-    {
-        SetTotalEnemy (--_enemyCounter);
-        if (_enemyCounter < 0)
-        {
-            bool isAllEnemyDestroyed = _spawnedEnemies.Find (e => e.gameObject.activeSelf) == null;
-            if (isAllEnemyDestroyed)
-            {
-                SetGameOver (true);
+    private void SpawnEnemy() {
+        SetTotalEnemy(--_enemyCounter);
+
+        if (_enemyCounter < 0) {
+            bool isAllEnemyDestroyed = _spawnedEnemies.Find(e => e.gameObject.activeSelf) == null;
+
+            if (isAllEnemyDestroyed) {
+                SetGameOver(true);
             }
 
             return;
         }
 
-        int randomIndex = Random.Range (0, _enemyPrefabs.Length);
-        string enemyIndexString = (randomIndex + 1).ToString ();
+        int randomIndex = Random.Range(0, _enemyPrefabs.Length);
+        string enemyIndexString = (randomIndex + 1).ToString();
 
-        GameObject newEnemyObj = _spawnedEnemies.Find (
-            e => !e.gameObject.activeSelf && e.name.Contains (enemyIndexString)
+        GameObject newEnemyObj = _spawnedEnemies.Find(
+            e => !e.gameObject.activeSelf && e.name.Contains(enemyIndexString)
         )?.gameObject;
 
-        if (newEnemyObj == null)
-        {
-            newEnemyObj = Instantiate (_enemyPrefabs[randomIndex].gameObject);
+        if (newEnemyObj == null) {
+            newEnemyObj = Instantiate(_enemyPrefabs[randomIndex].gameObject);
         }
 
-        Enemy newEnemy = newEnemyObj.GetComponent<Enemy> ();
-        if (!_spawnedEnemies.Contains (newEnemy))
-        {
-            _spawnedEnemies.Add (newEnemy);
+        Enemy newEnemy = newEnemyObj.GetComponent<Enemy>();
+        if (!_spawnedEnemies.Contains(newEnemy)) {
+            _spawnedEnemies.Add(newEnemy);
         }
 
         newEnemy.transform.position = _enemyPaths[0].position;
-        newEnemy.SetTargetPosition (_enemyPaths[1].position);
-        newEnemy.SetCurrentPathIndex (1);
-        newEnemy.gameObject.SetActive (true);
+        newEnemy.SetTargetPos(_enemyPaths[1].position);
+        newEnemy.SetCurrentPathIndex(1);
+        newEnemy.gameObject.SetActive(true);
     }
 
-    public Bullet GetBulletFromPool (Bullet prefab)
-    {
-        GameObject newBulletObj = _spawnedBullets.Find (
-            b => !b.gameObject.activeSelf && b.name.Contains (prefab.name)
+    // Untuk menampilkan garis penghubung dalam window Scene
+    // tanpa harus di-Play terlebih dahulu
+    private void OnDrawGizmos() {
+        for (int i = 0; i < _enemyPaths.Length - 1; i++) {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawLine(_enemyPaths[i].position, _enemyPaths[i + 1].position);
+        }
+    }
+
+    public Bullet GetBulletFromPool(Bullet prefab) {
+        GameObject newBulletObj = _spawnedBullets.Find(
+            b => !b.gameObject.activeSelf && b.name.Contains(prefab.name)
         )?.gameObject;
 
-        if (newBulletObj == null)
-        {
-            newBulletObj = Instantiate (prefab.gameObject);
+        if (newBulletObj == null) {
+            newBulletObj = Instantiate(prefab.gameObject);
         }
 
-        Bullet newBullet = newBulletObj.GetComponent<Bullet> ();
-        if (!_spawnedBullets.Contains (newBullet))
-        {
-            _spawnedBullets.Add (newBullet);
+        Bullet newBullet = newBulletObj.GetComponent<Bullet>();
+
+        if (!_spawnedBullets.Contains(newBullet)) {
+            _spawnedBullets.Add(newBullet);
         }
 
         return newBullet;
     }
 
-    public void ExplodeAt (Vector2 point, float radius, int damage)
-    {
-        foreach (Enemy enemy in _spawnedEnemies)
-        {
-            if (enemy.gameObject.activeSelf)
-            {
-                if (Vector2.Distance (enemy.transform.position, point) <= radius)
-                {
-                    enemy.ReduceEnemyHealth (damage);
+    public void ExplodeAt(Vector2 point, float radius, int damage) {
+        foreach (Enemy enemy in _spawnedEnemies) {
+            if (enemy.gameObject.activeSelf) {
+                if (Vector2.Distance(enemy.transform.position, point) <= radius) {
+                    enemy.ReduceEnemyHealth(damage);
                 }
             }
         }
     }
 
-    public void ReduceLives (int value)
-    {
-        SetCurrentLives (_currentLives - value);
-        if (_currentLives <= 0)
-        {
-            SetGameOver (false);
+    public void ReduceLives(int value) {
+        SetCurrentLives(_currentLives - value);
+
+        if (_currentLives <= 0) {
+            SetGameOver(false);
         }
     }
 
-    public void SetCurrentLives (int currentLives)
-    {
+    public void SetCurrentLives(int currentLives) {
         // Mathf.Max fungsi nya adalah mengambil angka terbesar
         // sehingga _currentLives di sini tidak akan lebih kecil dari 0
-        _currentLives = Mathf.Max (currentLives, 0);
+        _currentLives = Mathf.Max(currentLives, 0);
         _livesInfo.text = $"Lives: {_currentLives}";
     }
 
-    public void SetTotalEnemy (int totalEnemy)
-    {
+    public void SetTotalEnemy(int totalEnemy) {
         _enemyCounter = totalEnemy;
-        _totalEnemyInfo.text = $"Total Enemy: {Mathf.Max (_enemyCounter, 0)}";
+        _totalEnemyInfo.text = $"Total Enemy: {Mathf.Max(_enemyCounter, 0)}";
     }
 
-    public void SetGameOver (bool isWin)
-    {
+    public void SetGameOver(bool isWin) {
         IsOver = true;
+
         _statusInfo.text = isWin ? "You Win!" : "You Lose!";
-        _panel.gameObject.SetActive (true);
+        _panel.gameObject.SetActive(true);
     }
-
-    // Untuk menampilkan garis penghubung dalam window Scene
-    // tanpa harus di-Play terlebih dahulu
-    private void OnDrawGizmos ()
-    {
-        for (int i = 0; i < _enemyPaths.Length - 1; i++)
-        {
-            Gizmos.color = Color.cyan;
-            Gizmos.DrawLine (_enemyPaths[i].position, _enemyPaths[i + 1].position);
-        }
-    }
-
-    
 }
